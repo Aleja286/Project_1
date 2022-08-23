d54eadd6d7b9c4108a2eee64f65cd44c847102bb
Aleja286 <d.preciador@uniandes.edu.co>
2022-08-22 23:36:56 -0500

    script_2

---
+++ script_2.sh
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+x_1= input("Digite el primer número")
+x_2= input("Digite el segundo número número")
+
+def plus_parametros(x_1,x_2):
+    multi= int(x_1)*int(x_2)
+    return multi
+
+Producto= plus_parametros(x_1, x_2)
+print(Producto)
+
