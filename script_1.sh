ecc37d14dbfd8980af81ae88bbd99288f196e180
Aleja286 <d.preciador@uniandes.edu.co>
2022-08-22 23:33:28 -0500

    first tracking

---
+++ script_1.sh
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+x_1= input("Digite el primer número")
+x_2= input("Digite el segundo número número")
+
+def suma_parametros(x_1,x_2):
+    suma= int(x_1)+int(x_2)
+    return suma
+
+Respuesta= suma_parametros(x_1, x_2)
+print(Respuesta)
+
